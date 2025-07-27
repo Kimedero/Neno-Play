069127fa9a5bfe4f9fcf7119913de48936f1d990
Kimedero <50914712+Kimedero@users.noreply.github.com>
2025-07-27 10:50:19 +0300

    I made it work
    But I don't fee like I understand why. And there's some functionality like dragging from outside the buttons works but once a button is selected you cannot drag from outside again until you select another button

--- TESTS/investigating_touch.gd
+++ TESTS/investigating_touch.gd
@@ -11,9 +11,17 @@ extends Node2D
 @onready var dot_line_2d: Line2D = $DotLine2D
 
 @onready var word_label: Label = $WordLabel
+@onready var word_label_2: Label = $WordLabel2
+
 
 var current_dot: Area2D
 var last_dot: Area2D
+
+var selected_dots_array: Array
+
+var drag_started: bool
+var line_start_point: Vector2
+var line_coordinates_array: Array
 
 func _ready() -> void:
 	for dt in dots_array:
@@ -21,8 +29,8 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	#word_label.text = "Screen Touch: %s" % [screen_touch.pressed]
-	pass
+	word_label_2.text = "Selected Dot Array Size: %s\nLine Coords Array: %s" % [selected_dots_array.size(), line_coordinates_array.size()]
+
 
 
 func _input(event: InputEvent) -> void:
@@ -36,30 +44,77 @@ func _input(event: InputEvent) -> void:
 
 
 #func process_drag(screen_drag: InputEventScreenDrag):
-	#word_label.text = "Screen Drag Pos\n%s" % [screen_drag.position]
-	#print(screen_drag)
+	##word_label.text = "Screen Drag Pos\n%s" % [screen_drag.position]
+	##print(screen_drag)
+	#if screen_drag:
+		#drag_started = true
+	##if drag_started:
+		#dot_line_2d.clear_points()
+		#
+		##if not line_start_point == Vector2.ZERO:
+		#dot_line_2d.add_point(line_start_point)
+		#for point in line_coordinates_array:
+			#dot_line_2d.add_point(point)
+		#dot_line_2d.add_point(screen_drag.position)
 
 
 func process_touch(screen_touch: InputEventScreenTouch):
-	# when we release the screen we also invoke the mouse exit
+	# when we release the screen we revert all sprites to default
 	if screen_touch.pressed == false:
 		for dt in dots_array:
 			dot_deactivate(dt)
-		print('All deactivated!')
+		selected_dots_array.clear()
+		#print("Seleted dots array cleared!")
+		line_coordinates_array.clear()
+		dot_line_2d.clear_points()
+		#line_start_point = Vector2.ZERO
+		drag_started = false
+		#print('All deactivated!')
 
 
 func on_input_event(viewport: Node, event: InputEvent, shape_idx: int, dot: Area2D):
-	if event is InputEventScreenTouch:
-		print("Input Event -> Viewport: %s - Event: %s - Shape Index: %s - Dot: %s" % [viewport, event, shape_idx, dot])
+	if event is InputEventScreenTouch and event.index == 0:
+		#print("Input Event -> Viewport: %s - Event: %s - Shape Index: %s - Dot: %s" % [viewport, event, shape_idx, dot])
 		if event.pressed:
+			if dot not in selected_dots_array:
+				selected_dots_array.append(dot)
+				
 			dot_activate(dot)
+			
+			display_formed_word()
+			
+			line_start_point = dot.position
+			drag_started = true
 		elif not event.pressed:
 			dot_deactivate(dot)
+			
+			
+	elif event is InputEventScreenDrag and event.index == 0:
+		if dot != last_dot and dot not in selected_dots_array:
+			selected_dots_array.append(dot)
+			
+			display_formed_word()
+			
+			line_coordinates_array.append(dot.position)
+			
+			dot_activate(dot)
+			print("Drag Event: %s" % [event])
+		last_dot = dot
+
 
 func dot_activate(dot: Area2D):
 	dot.get_node('Sprite2D').modulate = Color.ORANGE_RED
+	#word_label.text = "Activated Dot: %s" % [dot.name]
 
 
 func dot_deactivate(dot: Area2D):
 	dot.get_node('Sprite2D').modulate = Color.DEEP_SKY_BLUE
-	dot = null
+	#word_label.text = "Deactivated Dot: %s" % [dot.name]
+
+
+func display_formed_word():
+	word_label.text = ""
+	for dt in selected_dots_array:
+		var letter = dt.get_node('Label').text
+		word_label.text += letter
+		
